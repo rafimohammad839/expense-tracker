46a6beca277f97da6aa0e5089e7447699d7e5aab
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-11-18 12:25:27 +0530

    initial

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../contexts/GlobalState';
+
+const AddTransaction = () => {
+    const { addTransaction, transactions } = useContext(GlobalContext);
+
+    const [text, setText] = useState("");
+    const [amount, setAmount] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let newId = Math.random() * 100000;
+
+        let newTransaction = {
+            id: newId,
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction);
+        setText("")
+        setAmount(0)
+    }
+
+    return (
+        <>
+            <h3>Add new transaction</h3>   
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
+                    <input type="text" value={text} onChange={e => setText(e.target.value)} placeholder='Enter text...'/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">Amount (negative - expense, positive - income)</label>
+                    <input type="number" value={amount} onChange={e => setAmount(e.target.value)} placeholder='Enter amount...'/>
+                </div>
+                <button className="btn">Add transaction</button>
+            </form>
+        </>
+    )
+}
+
+export default AddTransaction
---
+++ src/components/TransactionList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../contexts/GlobalState';
+import Transaction from './Transaction';
+
+const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    return (
+        <>
+            <h3>History</h3>   
+            <ul className="list">
+                {transactions.map(transaction => {
+                    return (
+                        <Transaction transaction={transaction} key={transaction.id} />
+                    )
+                })}
+                
+            </ul>   
+        </>
+    )
+}
+
+export default TransactionList
